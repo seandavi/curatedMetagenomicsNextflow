74226bf4680a111a897a7c41b74aabf0d524ca45
Sean Davis <seandavi@br018.pvt.bridges.psc.edu>
2021-01-04 09:15:41 -0500

    working xsede/bridges single-sample

--- submit_bridges.sh
+++ submit_bridges.sh
@@ -3,9 +3,11 @@ set -x
 
 module load singularity
 module load git
-export GOOGLE_APPLICATION_CREDENTIALS=/home/seandavi/curatedmetagenomicdata-f056e9ef05f6.jsonexport 
+export GOOGLE_APPLICATION_CREDENTIALS=/home/seandavi/curatedmetagenomicdata-f056e9ef05f6.json
 
-WORKDIR=$LOCAL/
+WORKDIR=$LOCAL
+
+mkdir -p $WORKDIR
 
 echo "using WORKDIR $WORKDIR"
 
@@ -15,4 +17,7 @@ cp nextflow $WORKDIR
 cd $WORKDIR
 export NXF_MODE=google
 #NXF_MODE=google nextflow run main.nf -with-weblog https://cmgd-telemetry-whnnxetv4q-uc.a.run.app/nextflow/events -profile nih_slurm --metadata_tsv $1 && cd .. && rm -rf $WORKDIR
+ls
+pwd
 NXF_MODE=google ./nextflow run main.nf -with-weblog https://cmgd-telemetry-whnnxetv4q-uc.a.run.app/nextflow/events -profile bridges_node_only --srr $1 --uuid $2 
+sleep 360

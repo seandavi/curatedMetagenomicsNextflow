e052f95921a009a96aeb9bcac483e6943dc66f10
Sean Davis <seandavi@gmail.com>
2021-09-29 08:44:14 -0600

    add runthrough script

---
+++ runthrough.sh
@@ -0,0 +1,26 @@
+
+# install nextflow
+export NXF_MODE=google
+curl https://get.nextflow.io | bash
+
+# gsutil if you like
+# pip install gsutil
+
+# setup google access
+export GOOGLE_APPLICATION_CREDENTIALS=/Users/seandavis/Documents/git/curatedMetagenomicsNextflow/curatedmetagenomicdata-0ca253fe73b7.json
+
+# where to put stuff
+export GOOGLE_BUCKET_NAME=temp-testing-cmgd
+# make bucket if it doesn't exist
+gsutil mb gs://$GOOGLE_BUCKET_NAME
+
+# pull the workflow directly from github
+nextflow pull seandavi/curatedMetagenomicsNextflow -r main # could be a specific revision
+
+# And run with sample data
+nextflow run seandavi/curatedMetagenomicsNextflow \
+  -profile google \
+  -work-dir gs://$GOOGLE_BUCKET_NAME/work \
+  -r main \
+  --metadata_tsv https://raw.githubusercontent.com/seandavi/curatedMetagenomicsNextflow/main/samplesheet.test.tsv \
+  --bucket gs://$GOOGLE_BUCKET_NAME

03a3f65147a1556804b0cb8ca886726f015ff515
Sean Davis <seandavi@gmail.com>
2024-04-20 12:15:18 -0400

    anvil submit script cleanup

--- submit_anvil.sh
+++ submit_anvil.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --mem=48G
 #SBATCH --cpus-per-task=16
-#SBATCH --time=2:00:00
+#SBATCH --time=24:00:00
 set -x
 
 NXF_SINGULARITY_CACHEDIR=$SCRATCH/singularity_cache
@@ -13,8 +13,8 @@ mkdir -p $WORKDIR
 
 echo "working in $WORKDIR"
 
-cp main.nf $WORKDIR
-cp nextflow.config $WORKDIR
+#cp main.nf $WORKDIR
+#cp nextflow.config $WORKDIR
 
 
 export GOOGLE_APPLICATION_CREDENTIALS=$HOME/omicidx-338300-cbd1527c319e.json
@@ -22,9 +22,9 @@ module load nextflow
 
 cd $WORKDIR
 export NXF_MODE=google
-nextflow run main.nf --run_ids=$1 --sample_id=$2 -profile anvil
+nextflow run seandavi/curatedMetagenomicsNextflow --run_ids=$1 --sample_id=$2 -profile anvil
 
 cd $HOME
 
-# cleanup
-#rm -r $WORKDIR
+cleanup
+rm -r $WORKDIR

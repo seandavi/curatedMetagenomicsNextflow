3078a8e7efe4c91328cc6876d41c6cf7855e6f6f
Sean Davis <seandavi@gmail.com>
2025-03-22 19:06:52 -0400

    Update alpine submit script

--- submit_alpine.sh
+++ submit_alpine.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --mem=48G
+#SBATCH --mem=64G
 #SBATCH --cpus-per-task=16
 #SBATCH --time=24:00:00
 
@@ -21,4 +21,4 @@ module load nextflow
 cd $SLURM_SCRATCH
 export NXF_MODE=google
 #nextflow run main.nf --run_ids=$1 --sample_id=$2 -profile alpine
-nextflow run seandavi/curatedMetagenomicsNextflow --metadata_tsv=$1 -profile alpine -with-weblog https://nf-telemetry-819875667022.us-central1.run.app/nextflow-telemetry/events
+nextflow run seandavi/curatedMetagenomicsNextflow --run_ids=$1 --sample_id=$2 -profile alpine
